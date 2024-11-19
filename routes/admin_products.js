b29a545be7d799301aa9552acbfd05c264a317c0
Danieltlacuache <eguzmanseda@gmail.com>
2024-11-19 11:33:49 -0600

    Commit Emilio

---
+++ routes/admin_products.js
@@ -0,0 +1,134 @@
+const express = require('express');
+
+const router = express.Router();
+
+const {cargar_parsear_productos, guardar_productos} = require('../data_handler');
+
+const { generateUUID } = require('../app/controllers/utils');
+
+// Middleware para validar si el usuario es administrador
+const validateAdmin = (req, res, next) => {
+    console.log('Headers recibidos:', req.headers);
+    const authHeader = req.headers['x-auth'];
+    console.log('validateAdmin middleware:', authHeader); 
+    if (authHeader !== 'admin') {
+        return res.status(403).json({ message: "Acceso no autorizado, no se cuenta con privilegios de administrador." });
+    }
+    next();
+};
+
+/*
+// Ruta GET para obtener todos los productos (solo para administradores)
+router.get('/', validateAdmin, async (req, res) => {
+    try {
+        // Cargar y parsear los productos desde el archivo de datos
+        const products = await cargar_parsear_productos();
+        res.status(200).json(products);
+    } catch (error) {
+        console.error("Error al obtener los productos: ", error);
+        res.status(500).json({ message: "Error al obtener los productos." });
+    }
+});
+
+*/
+
+// Ruta POST para agregar un nuevo producto (solo para administradores)
+router.post('/', validateAdmin, async (req, res) => {
+    const newProduct = req.body; // Producto enviado en el cuerpo de la solicitud
+
+    // Validar los campos obligatorios del producto
+    if (!newProduct || !newProduct.title || !newProduct.price) {
+        return res.status(400).json({ message: "Datos del producto incompletos. Se requieren 'title' y 'price'." });
+    }
+
+    try {
+        // Cargar los productos actuales desde el archivo de datos
+        const products = await cargar_parsear_productos();
+
+        // Generar un nuevo UUID para el producto
+        newProduct.uuid = newProduct.uuid || generateUUID();
+
+        // Agregar el nuevo producto al arreglo
+        products.push(newProduct);
+
+        // Guardar el arreglo de productos actualizado
+        await guardar_productos(products);
+
+        // Responder con éxito
+        res.status(201).json({ message: "Producto agregado exitosamente.", product: newProduct });
+    } catch (error) {
+        console.error("Error al agregar el producto: ", error);
+        res.status(500).json({ message: "Error al agregar el producto." });
+    }
+});
+
+//Ruta PUT para modificar los atributs de algún producto mediante id 
+
+router.put('/:id', validateAdmin, async (req, res) => {
+    const actualizar_product = req.body; // Producto enviado en el cuerpo de la solicitud
+    const {id} = req.params;
+
+
+    // Validar los campos obligatorios del producto
+    if (!actualizar_product || !actualizar_product.title || !actualizar_product.price) {
+        return res.status(400).json({ message: "Datos del producto incompletos. Se requieren 'title' y 'price'." });
+    }
+
+    try {
+        // Cargar los productos actuales desde el archivo de datos
+        const products = await cargar_parsear_productos();
+
+        const product_indice = products.findIndex( p => p.uuid == id);
+
+        //como uso findIndex, si el producto con el ID a buscar no se encuentra, regresa -1, por eso así hice la comprobación 
+        if(product_indice === -1){
+            return res.status(404).json({message: "Producto no encontrado"});
+        }
+
+        // Actualizar el producto existente
+        products[product_indice] = { ...products[product_indice], ...actualizar_product};
+
+        // Guardar el arreglo de productos actualizado
+        await guardar_productos(products);
+
+        // Responder con éxito
+        res.status(200).json({ message: "Producto actualizado exitosamente.", product: products[product_indice] });
+    } catch (error) {
+        console.error("Error al actualizar el producto: ", error);
+        res.status(500).json({ message: "Error al actualizar el producto." });
+    }
+});
+
+//Ruta DELETE para eliminar un producto mediante id 
+router.delete('/:id', validateAdmin, async(req,res) =>{
+    const {id} = req.params; //uso params para obtener el id del producto a eliminar 
+
+    try{
+            // Cargar los productos actuales desde el archivo de datos
+            const products = await cargar_parsear_productos();
+
+            const product_indice = products.findIndex( p => p.uuid == id);
+
+            //como uso findIndex, si el producto con el ID a buscar no se encuentra, regresa -1, por eso así hice la comprobación 
+            if(product_indice === -1){
+                return res.status(404).json({message: "Producto no encontrado"});
+            }
+
+           // Eliminar el producto del arreglo
+            const [producto_a_eliminar] = products.splice(product_indice,1);
+
+           // Guardar el arreglo de productos actualizado
+            await guardar_productos(products);
+
+        // Responder con éxito
+        res.status(200).json({ message: "Producto eliminado exitosamente.", product: producto_a_eliminar});
+    }catch(error){
+        console.error("Error al eliminar el producto: ", error);
+        res.status(500).json({ message: "Error al  eliminar el producto." });
+    }
+
+});
+
+
+
+module.exports = {router, validateAdmin};
---
+++ server.js
@@ -0,0 +1,83 @@
+//importación de express 
+const express = require('express');
+
+const path = require('path');
+
+//importación de router.js
+
+const router = require('./app/controllers/router');
+
+//import de la función declarada para cargar y parsear el archivo products.json
+// y como regreso un objeto en la función por eso lo pongo entre llaves, para guardar el parseo 
+//const {cargar_parsear_productos} = require('./data_handler');
+
+//declaración de app, variable la cual va a ser middleware
+const app = express();
+//middleware que dice el profe que es para parsear el JSON
+app.use(express.json());
+
+//uso de router como middleware global
+
+/* COmento estas rutas, como indica el profe, para que solo quede la ruta de inicilización
+
+//Declaración de ruta raíz
+app.get('/', (req,res) =>{
+    res.send('e-commerce app práctica 3 :)');
+});
+
+//Ruta que voy a declarar parqa obtener los productos
+app.get('/products', async(req,res) => {
+    try{
+        const products = await cargar_parsear_productos();
+        res.json(products);
+    }catch(error){
+        res.status(500).send("Error a la hora de obtener los productos");
+    }
+});
+
+*/
+//console.log("Middleware de archivos ");
+app.use('/', router);
+
+app.use(express.static('app'));
+
+//app.use('/views', express.static('views'));
+
+
+//para probar 
+// 
+app.use(express.static(path.join(__dirname)));
+
+// 
+app.use('/app', express.static(path.join(__dirname, 'app')));
+
+// 
+app.use('/views', express.static(path.join(__dirname, 'views')));
+
+// 
+app.use('/api', router);
+
+// 
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views', 'P01_index.html'));
+});
+
+// 
+app.get('/app/home.js', (req, res) => {
+    res.sendFile(path.join(__dirname, 'app', 'home.js'));
+});
+
+// Si estás sirviendo una ruta específica para acceder a un archivo estático, puedes hacerlo así:
+app.get('/app/controllers/carrito', (req, res) => {
+    res.sendFile(path.join(__dirname, 'app', 'controllers', 'carrito.js'));
+});
+
+
+console.log("Middleware de archivos estáticos configurado");
+//Ahora sí, levantar el servidor en el puerto 3000 como lo pide el profe 
+const puerto = 3000;
+app.listen(puerto, () => {
+    console.log("Servidor corriendo en http://localhost:3000");
+})
+
+//SI sirvió!!!!!!! :)
